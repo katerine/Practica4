a9f8d62047854aa172c18edd1546c6b91a19d437
katerine <katerine_cardona@hotmail.com>
2012-10-13 22:14:30 +0100

    second commit

--- class_circunferencia.rb
+++ class_circunferencia.rb
@@ -9,3 +9,26 @@ class Circunferencia
   end
   
 end
+
+require "circunferencia"
+require "test/unit"
+
+class TestCircunferencia < Test::Unit::TestCase
+  
+  def test_calculo_radio_ok 
+    assert_equal(2, Circunferencia.new(12.56).radio())
+  end
+  
+  def test_negativo
+    assert_raise(RuntimeError,"No puede haber perC-metros o radios negativos") {Circunferencia.new(-50).radio()}
+  end
+  
+  def test_no_numero
+    assert_raise(RuntimeError,"El perC-metro debe ser un nC:mero positivo") {Circunferencia.new("blabla").radio()}
+  end
+  
+  def test_calculo_radio_ko 
+    assert_equal(5, Circunferencia.new(12.56).radio())
+  end
+  
+end
